63c5551951658c0897c00e22d2dc8c0d10089dcd
Inaki Anduaga <inaki.anduaga@tado.com>
2016-10-13 00:08:42 +0200

    #1: Got JSX parsing working through babel

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import xs from 'xstream';
 // import {h1} from '@cycle/dom';
 const { html } = require('snabbdom-jsx');
-const JSX = {createElement: html};
 
 export default function Main() {
   const sinks = {
-    DOM: xs.periodic(1000).map(i =>
-      <h1>{ i } seconds </h1>
-    )
+    DOM: xs.periodic(1000).map(i => <div>{ i } seconds </div>)
   };
   return sinks;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
       {
         test: /\.tsx?$/,
         loaders: [
+          'babel?cacheDirectory',
           'awesome-typescript-loader'
         ],
       },
